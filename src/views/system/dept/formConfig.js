18915bf1ef8acf3b388b5efa53cc3bf84586e5b9
yanglin <alp_yanglin@163.com>
2021-11-18 14:07:45 +0800

    部门管理

--- src/views/system/dept/formConfig.js
+++ src/views/system/dept/formConfig.js
@@ -1,38 +1,3 @@
-const topForm = {
-  titleShow: false, //  表单块标题是否隐藏
-  labelAlign: "left", // label对齐方式 left|right,
-  layout: "inline", // 表单布局 'horizontal'|'vertical'|'inline'  
-  hideRequiredMark: true, // 是否隐藏必选标记
-  // a_attr: {}, //  原antd组件属性
-  colon: true, // layout = horizontal 有效 label后是否有冒号
-  // gutter: 10, // 表单块间隔
-  form: [
-    {
-      label: "部门名称",
-      name: 'postName',
-      placeholder: '请输入岗位名称',
-      value: '',
-      type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom
-    },{
-      label: "状态",
-      name: 'status',
-      placeholder: '岗位状态',
-      value: '',
-      options: [
-        { label: '正常', value: '0' },
-        { label: '停用', value: '1' },
-      ],
-      a_attr: {
-        allowClear: true,
-      },
-      type: 'select', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom
-    },{
-      type: 'custom',
-      soltName: 'topBtns'
-    }
-  ]
-}
-
 const mainForm = {
   titleShow: false, //  表单块标题是否隐藏
   labelAlign: "right", // label对齐方式 left|right,
@@ -42,25 +7,27 @@ const mainForm = {
     labelCol: { style: { width: '70px' } }
   }, //  原antd组件属性
   colon: false, // layout = horizontal 有效 label后是否有冒号
-  // gutter: 10, // 表单块间隔
+  gutter: 10, // 表单块间隔
   form: [
     {
-      label: "postId",
-      name: 'postId',
+      label: "deptId",
+      name: 'deptId',
       value: null,
       hidden: true,
-      span: 24,
+      span: 12,
       type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
     },{
       type: 'custom',
       soltName: 'deptTree',
-      span: 24,
+      span: 12,
+      offset: 3,
     },{
       label: "部门名称",
-      name: 'postCode',
+      name: 'deptName',
       placeholder: '请输入部门名称',
       value: '',
       span: 12,
+      offset: 3,
       rule: [
         { 
           required: true,
@@ -70,10 +37,11 @@ const mainForm = {
       type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
     },{
       label: "显示顺序",
-      name: 'postSort',
+      name: 'orderNum',
       placeholder: '',
       value: '',
       span: 12,
+      offset: 3,
       a_attr:{
         type: 'number',
       },
@@ -86,65 +54,55 @@ const mainForm = {
       type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
     },{
       label: "负责人",
-      name: 'postSort',
+      name: 'leader',
       placeholder: '',
       value: '',
       span: 12,
+      offset: 3,
       a_attr:{
       },
       type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
     },{
       label: "联系电话",
-      name: 'postSort',
+      name: 'phone',
       placeholder: '',
       value: '',
       span: 12,
+      offset: 3,
       a_attr:{
         type: 'number',
       },
       type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
     },{
       label: "邮箱",
-      name: 'postSort',
+      name: 'email',
       placeholder: '',
       value: '',
       span: 12,
+      offset: 3,
       a_attr:{
-        type: 'number',
       },
-      rule: [
-        { 
-          required: true,
-          message: '显示顺序不能为空',
-        },
-      ],
       type: 'input', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
-    }
-    
-    ,{
-      label: "岗位状态",
+    },{
+      label: "部门状态",
       name: 'status',
       value: '',
-      span: 24,
+      span: 12,
+      offset: 3,
       options: [
         { label: '正常', value: '0' },
         { label: '停用', value: '1' },
       ],
       type: 'radioGroup', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
     },{
-      label: "备注",
-      name: 'remark',
-      value: '',
-      span: 24,
-      a_attr: {
-        rows: "4"
-      },
-      type: 'textarea', // input/select/dateMonth/dateWeek/dateTime/radioGroup/treeSelect/checkboxGroup/custom/textarea 
-    }
+      type: 'custom',
+      soltName: 'formBtn',
+      span: 12,
+      offset: 3,
+    },
   ]
 }
 
 export default {
-  topForm,
   mainForm,
 }
--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -1,78 +1,159 @@
 <template>
   <div class="systemDeptPage">
-    <a-row :gutter="10">
-      <a-col :span="6">
-        <div class="treeView">
-          <div class="search">
-            <a-input-search v-model:value="searchTreeKey" placeholder="关键字" />
-          </div>
-          <div class="tree">
-            <a-tree
-              :blockNode="true"
-              :showIcon="false"
-              :showLine="true"
-              :replaceFields="treeField"
-              size="small"
-              :expandedKeys="expandedKeys"
-              :auto-expand-parent="autoExpandParent"
-              :tree-data="treeData"
-              @expand="onTreeExpand"
-              @select="selTreeNode"
-            >
-              <template #switcherIcon><down-outlined /></template>
-              <template #title="scope">
-                <a-dropdown :trigger="['contextmenu']">
-                  <span v-if="scope.deptName.indexOf(searchTreeKey) > -1">
-                    {{scope.deptName.substr(0, scope.deptName.indexOf(searchTreeKey))}}<span style="color: #f50">{{ searchTreeKey }}</span>{{scope.deptName.substr(scope.deptName.indexOf(searchTreeKey) + searchTreeKey.length)}}
-                  </span>
-                  <span v-else>{{ scope.deptName }}</span>
+    <div class="treeView">
+      <div class="search">
+        <a-input-search v-model:value="searchTreeKey" placeholder="关键字" />
+      </div>
+      <div class="tree">
+        <a-tree
+          :blockNode="true"
+          :showIcon="false"
+          :showLine="true"
+          :replaceFields="treeField"
+          size="small"
+          :expandedKeys="expandedKeys"
+          :auto-expand-parent="autoExpandParent"
+          :tree-data="treeData"
+          @expand="onTreeExpand"
+          @select="selTreeNode"
+        >
+          <template #switcherIcon><down-outlined /></template>
+          <template #title="scope">
+            <a-dropdown :trigger="['contextmenu']">
+              <span v-if="scope.deptName.indexOf(searchTreeKey) > -1">
+                {{
+                  scope.deptName.substr(
+                    0,
+                    scope.deptName.indexOf(searchTreeKey)
+                  )
+                }}<span style="color: #f50">{{ searchTreeKey }}</span
+                >{{
+                  scope.deptName.substr(
+                    scope.deptName.indexOf(searchTreeKey) + searchTreeKey.length
+                  )
+                }}
+              </span>
+              <span v-else>{{ scope.deptName }}</span>
 
-                  <template #overlay>
-                    <a-menu size="small" @click="({ key: menuKey }) => treeNodeContextMenuClick(scope, menuKey)">
-                      <a-menu-item key="add">添加</a-menu-item>
-                      <a-menu-item key="del">删除</a-menu-item>
-                    </a-menu>
-                  </template>
-                </a-dropdown>
+              <template #overlay>
+                <a-menu
+                  size="small"
+                  @click="
+                    ({ key: menuKey }) =>
+                      treeNodeContextMenuClick(scope, menuKey)
+                  "
+                >
+                  <a-menu-item key="add">添加</a-menu-item>
+                  <a-menu-item v-if="scope.parentId!=0 && scope.children.length==0" key="del">删除</a-menu-item>
+                </a-menu>
               </template>
-            </a-tree>
-          </div>
-        </div>
-      </a-col>
-      <a-col :span="18">
-        <div class="mainView">
-
-        </div>
-      </a-col>
-    </a-row>
+            </a-dropdown>
+          </template>
+        </a-tree>
+      </div>
+    </div>
+    <div class="mainView">
+      <a-alert
+        v-if="deptFormType==0"
+        message="尚未指定一个部门"
+        type="warning"
+      />
+      <div class="formView" v-else>
+        <div class="tit">{{deptFormType==1?'新增部门':'修改部门'}}</div>
+        <alpFormGroup
+          ref="deptForm"
+          :options="deptForm"
+          :form_data="deptFormData"
+          :key="formUpdataKey"
+        >
+          <template #deptTree>
+            <div class="deptTree">
+              <div class="label">上级部门</div>
+              <div class="setInput" :class="{ err: deptFormParentIdErr }">
+                <a-tree-select
+                  v-model:value="deptFormParentId"
+                  style="width: 100%"
+                  show-search
+                  allow-clear
+                  tree-default-expand-all
+                  :dropdown-style="{ maxHeight: '300px', overflow: 'auto' }"
+                  :tree-data="showDeptFormTree"
+                  :replaceFields="treeField"
+                  @change="checkDeptParentId"
+                >
+                </a-tree-select>
+                <span class="errMsg">上级部门不能为空</span>
+              </div>
+            </div>
+          </template>
+          <template #formBtn>
+            <div class="formBtn">
+              <a-button
+                type="primary"
+                @click="submitDept('add')"
+                style="margin-left: 70px"
+                >保存</a-button
+              >
+            </div>
+          </template>
+        </alpFormGroup>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, createVNode } from "vue";
+import { ExclamationCircleOutlined } from "@ant-design/icons-vue";
+// 部门表单配置
+import deptFormConfig from "./formConfig.js";
 
 export default defineComponent({
-  name: 'systemPostPage',
-  data: function(){
+  name: "systemPostPage",
+  data: function () {
     return {
       expandedKeys: [],
-      searchTreeKey: '',
+      searchTreeKey: "",
       autoExpandParent: true,
       treeData: [],
       oneDimData: [],
       treeField: {
-        children: 'children', 
-        title: 'deptName', 
-        key: 'deptId',
+        children: "children",
+        title: "deptName",
+        key: "deptId",
+        value: "deptId",
       },
-    }
+      // 上级部门下拉树
+      deptFormTree: [
+        {
+          createBy: "admin",
+          delFlag: "0",
+          deptId: "0",
+          deptName: "根目录",
+          email: "",
+          leader: "",
+          orderNum: "0",
+          parentId: "null",
+          phone: "",
+          status: "0",
+          children: [],
+        },
+      ],
+      showDeptFormTree: [],
+      deptFormParentId: "",
+      deptFormParentIdErr: false,
+      deptForm: deptFormConfig.mainForm,
+      deptFormData: {},
+      deptFormType: 0,  //  0 没有选取部门数据/1 新增数据/2  修改数据
+      formUpdataKey: 1,
+    };
   },
   watch: {
-    searchTreeKey: function(val){
+    searchTreeKey: function (val) {
       const expanded = <Array<any>>[];
-      this.oneDimData.forEach(item => {
-          if (item.deptName.indexOf(val) > -1) {
-            expanded.push(item.deptId);
-          }
+      this.oneDimData.forEach((item) => {
+        if (item.deptName.indexOf(val) > -1) {
+          expanded.push(item.deptId);
+        }
       });
       this.expandedKeys = expanded;
       this.searchTreeKey = val;
@@ -81,38 +162,43 @@ export default defineComponent({
   },
   methods: {
     // 获取部门树
-    getDeptTree: function(){
-      this.$axios.get('/system/dept/list').then(res => {
-        console.log(res)
-        if(res.data.code == 200){
-          var treeData = this.parseTreeData(res.data.data);
-          this.treeData = treeData;
-          this.oneDimData = res.data.data;
-        }else{
-          this.$message.error(res.data.msg);
-        }
-      }).catch(err => {
-        this.$message.error('服务器异常');
-      });
+    getDeptTree: function () {
+      this.$axios
+        .get("/system/dept/list")
+        .then((res) => {
+          // console.log(res);
+          if (res.data.code == 200) {
+            var treeData = this.parseTreeData(res.data.data);
+            this.treeData = treeData;
+            this.oneDimData = res.data.data;
+            // 部门下拉树
+            this.deptFormTree[0].children = treeData;
+          } else {
+            this.$message.error(res.data.msg);
+          }
+        })
+        .catch((err) => {
+          this.$message.error("服务器异常");
+        });
     },
     // 解析树结构
-    parseTreeData: function(data) {
-      var res = <any>[], map = {};
-      if(Array.isArray(data)){
-        data.sort((a,b) => a.deptId - b.deptId);
-        data.forEach(one => {
+    parseTreeData: function (data) {
+      var res = <any>[],
+        map = {};
+      if (Array.isArray(data)) {
+        data.sort((a, b) => a.deptId - b.deptId);
+        data.forEach((one) => {
           var template = {
             ...one,
             children: [],
           };
-          if(!map[one.parentId]){
+          if (!map[one.parentId]) {
             res.push(template);
-
-          }else{
+          } else {
             map[one.parentId].children.push(template);
           }
 
-          if(!map[one.deptId]){
+          if (!map[one.deptId]) {
             map[one.deptId] = template;
           }
         });
@@ -120,54 +206,260 @@ export default defineComponent({
       return res;
     },
     // 展开树节点
-    onTreeExpand: function(keys){
+    onTreeExpand: function (keys) {
       this.expandedKeys = keys;
       this.autoExpandParent = false;
     },
     // 树节点右键菜单
-    treeNodeContextMenuClick: function(data, menuKey) {
-      console.log(data, menuKey)
+    treeNodeContextMenuClick: function (data, menuKey) {
+      // console.log("menu", data, menuKey);
+      if (menuKey == "add") {
+        this.deptFormType = 1;
+        this.deptFormParentIdErr = false;
+        // 新增
+        this.deptFormData = {};
+        var treeData = JSON.parse(JSON.stringify(this.deptFormTree));
+        this.deptFormParentId = data.deptId;
+        this.showDeptFormTree = treeData;
+        this.formUpdataKey++;
+      } else if (menuKey == "del") {
+        // 删除
+        var that = this;
+        this.$confirm({
+          title: () => "删除",
+          icon: () => createVNode(ExclamationCircleOutlined),
+          content: () => "确定要删除吗？",
+          onOk() {
+            return new Promise((resolve) => {
+              // 删除
+              that.$axios.delete("/system/dept/"+data.deptId)
+                .then((res) => {
+                  resolve(true);
+                  if (res.data.code == 200) {
+                    that.getDeptTree();
+                    
+                    that.deptFormType = 0;
+                    that.deptFormData = {};
+                    that.deptFormParentId = '';
+                    that.deptFormParentIdErr = false;
+                    that.showDeptFormTree = [];
+                    that.formUpdataKey++;
+
+                  } else {
+                    that.$message.error(res.data.msg);
+                  }
+                })
+                .catch((err) => {
+                  that.$message.error("服务器异常");
+                  resolve(false);
+                });
+            });
+          },
+          onCancel() {},
+        });
+      }
     },
     // 选中树节点
-    selTreeNode: function(nodeKey) {
-      var targetNodeData = null;
-      for(var i=0;i<this.oneDimData.length;i++){
+    selTreeNode: function (nodeKey) {
+      var targetNodeData: any = null;
+      this.deptFormType = 2;
+      this.deptFormParentIdErr = false;
+      for (var i = 0; i < this.oneDimData.length; i++) {
         var one = this.oneDimData[i];
-        if(one.deptId == nodeKey){
+        if (one.deptId == nodeKey) {
           targetNodeData = one;
           break;
         }
       }
-      console.log(targetNodeData);
-      if(targetNodeData){
-        
+      this.deptFormData = {};
+      // 全部部门树数据
+      var treeData = JSON.parse(JSON.stringify(this.deptFormTree)),
+        deptId = null;
+
+      if (targetNodeData) {
+        var formKeys = [
+          "deptId",
+          "parentId",
+          "deptName",
+          "orderNum",
+          "leader",
+          "phone",
+          "email",
+          "status",
+        ];
+        formKeys.forEach((k) => {
+          this.deptFormData[k] = {
+            value: targetNodeData[k],
+          };
+          if (k == "parentId") {
+            this.deptFormParentId = targetNodeData[k];
+          }
+          if (k == "deptId") {
+            deptId = targetNodeData[k];
+          }
+        });
+        // 过滤部门下拉树
+        var res = this.$common.recursiveSearch({
+          data: treeData,
+          recursiveKey: "children",
+          searchKey: "deptId",
+          searchVal: this.deptFormParentId,
+        });
+        if (res.result) {
+          for (var i = res.data.children.length - 1; i >= 0; i--) {
+            var one = res.data.children[i];
+            if (one.deptId == deptId) {
+              res.data.children.splice(i, 1);
+            }
+          }
+        }
+        // 可展示变更树
+        this.showDeptFormTree = treeData;
+        this.formUpdataKey++;
       }
-    }
+    },
+
+    // 检验父级表单项的父级ID
+    checkDeptParentId: function () {
+      this.deptFormParentIdErr =
+        typeof this.deptFormParentId == "undefined" ||
+        this.deptFormParentId === "";
+      return this.deptFormParentIdErr;
+    },
+
+    // 提交部门保存
+    submitDept: function () {
+      var formTemplate = {
+        deptId: undefined,
+        parentId: this.deptFormParentId,
+        deptName: undefined,
+        orderNum: undefined,
+        leader: undefined,
+        phone: undefined,
+        email: undefined,
+        status: "0",
+      };
+
+      // 验证
+      if(this.checkDeptParentId()){
+        return;
+      }
+      var formRef = this.$refs['deptForm'];
+      formRef.formValidation().then(res => {
+        if(res.state){
+          var form = Object.assign(formTemplate, res.form);
+
+          // 发送请求
+          this.$axios[this.deptFormType == 1 ? "post" : "put"]("/system/dept", form)
+            .then((res) => {
+              if (res.data.code == 200) {
+                this.$message.success(this.deptFormType == 1?'添加成功': '修改成功');
+                // 获取部门树
+                this.getDeptTree();
+              } else {
+                this.$message.error(res.data.msg);
+              }
+            })
+            .catch((err) => {
+              this.$message.error("服务器异常");
+            });
+
+        }
+      });
+
+    },
   },
-  created: function() {
+  created: function () {
     // 获取部门树
     this.getDeptTree();
   },
-  setup() {
-    
-  },
-})
+});
 </script>
 <style lang="less">
-.systemDeptPage{
+.systemDeptPage {
+  min-width: 1000px;
   height: 100%;
   overflow: hidden;
-  >.ant-row{
+  background-color: #eee;
+
+  > .treeView {
+    padding: 10px;
+    float: left;
+    width: 300px;
+    background-color: #fff;
     height: 100%;
-    >.ant-col{
-      height: 100%;
-      background-color: #eee;
-      >.treeView,
-      >.mainView{
-        padding: 20px;
-        background-color: #fff;
-        height: 100%;
-        overflow: hidden;
+    overflow: auto;
+  }
+
+  > .mainView {
+    padding: 10px;
+    margin-left: 310px;
+    height: 100%;
+    overflow: auto;
+    background-color: #fff;
+    
+    > .formView {
+      padding-top: 100px; 
+      >.tit{
+        padding-left: 19%;
+        margin-bottom: 20px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 30px;
+        color: #333;
+      }
+
+      // 部门下拉树
+      .deptTree {
+        margin-bottom: 24px;
+        > .label {
+          float: left;
+          padding-right: 10px;
+          width: 70px;
+          height: 32px;
+          line-height: 32px;
+          overflow: hidden;
+          white-space: nowrap;
+          text-align: right;
+          vertical-align: middle;
+          color: rgba(0, 0, 0, 0.85);
+          font-size: 12px;
+
+          &:before {
+            display: inline-block;
+            margin-right: 4px;
+            color: #ff4d4f;
+            font-size: 12px;
+            font-family: SimSun, sans-serif;
+            line-height: 1;
+            content: "*";
+          }
+        }
+        > .setInput {
+          margin-left: 70px;
+          height: 32px;
+          line-height: 32px;
+          position: relative;
+          > span.errMsg {
+            display: none;
+            position: absolute;
+            top: 30px;
+            left: 0;
+            min-height: 24px;
+            font-size: 12px;
+            line-height: 24px;
+            color: #ff4d4f;
+          }
+
+          &.err {
+            .ant-select-selector {
+              border-color: #ff4d4f;
+            }
+            > span.errMsg {
+              display: block;
+            }
+          }
+        }
       }
     }
   }
