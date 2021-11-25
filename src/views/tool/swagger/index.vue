3432c49ccc757280e1a4b50adbb5ccaf38e02d27
A_orange <1504889935@qq.com>
2021-11-25 08:44:44 +0800

    系统接口

---
+++ src/views/tool/swagger/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tool-swagger">
+    <iframe
+      :src="docSrc"
+      frameborder="0"
+      :style="{
+        width: '100%',
+        height: containHeight + 'px'
+      }"
+    ></iframe>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'toolSwagger',
+  data() {
+    return {
+      // 容器高度
+      containHeight: 0,
+    }
+  },
+  computed: {
+    docSrc: function () {
+      return this.$serverConfig.serverUrl + '/doc.html'
+    }
+  },
+  mounted() {
+    this.containHeight = document.body.querySelector('.tool-swagger')?.clientHeight;
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.tool-swagger {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: #fff;
+}
+</style>
